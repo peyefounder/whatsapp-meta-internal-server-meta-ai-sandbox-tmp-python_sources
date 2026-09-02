792effbb36bd3c01a43264363d99ca0079f228c8
peyefounder <nurmalatamansari@gmail.com>
2026-09-02 13:17:17 +0700

    config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+DISABLE_NETWORK = True
+DISABLE_OS = True
+DISABLE_SOCKET = True
+DISABLE_SUBPROCESS = True
+DISABLE_DILL = True
+ALLOWED_MODULES = ['pandas', 'numpy', 'matplotlib', 'scipy', 'PIL', 'sympy', 'sklearn', 'io', 'json', 'ast', 'builtins']
